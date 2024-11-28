fb92a47c5e05b2f8409b27d10e1466138672c316
uriel07777 <uriel07777@gmail.com>
2024-11-28 12:42:12 +0200

    Refactor.

--- config/consts.py
+++ config/consts.py
@@ -1,7 +1,28 @@
+from enum import Enum
+from string import Template
 
-RSS_URL             = "https://subsplease.org/rss/?r=1080"  # The URL of the RSS feed
-RSS_FILE_PATH       = 'data/rss_data.json'                  # File to store RSS feed subscriptions
-OTAKU_CHANNEL_ID    = 571380049044045826                    # Channel ID where announcements will be sent
+class Statuses(Enum):
+    ALL_ANIME           = 0
+    CURRENTLY_WATCHING  = 1
+    COMPLETED           = 2
+    ON_HOLD             = 3
+    DROPPED             = 4
+    PLAN_TO_WATCH       = 6
+
+RSS_URL                 = "https://subsplease.org/rss/?r=1080"  # The URL of the RSS feed
+
+RSS_FILE_PATH           = 'data/rss_data.json'                  # File to store RSS feed subscriptions
+MAL_PROFILE_PATH        = 'data/mal_profile.txt'
+CONFIG_LOCAL_PATH       = 'config/config-local.json'
+CONFIG_PATH             = 'config/config.json'
+
+MAL_STATUSES_TEMPLATE   = Template('data/anime_list/$status.txt')
+MAL_LIST_TEMPLATE       = Template('data/anime_list/$status.txt')
+
+BOT_CHANNEL_ID          = 571462116612112384                # Channel ID where announcements will be sent
+OTAKU_CHANNEL_ID        = 571380049044045826                    # Channel ID where announcements will be sent
 
 DROPDOWN_TEXT_MAX_LEN   = 100   # Discord's limit per string in dropdown
 DROPDOWN_MAX_ITEMS      = 25    # Discord's limit per select menu
+MAX_LETTERS             = 2000
+
--- functions/mal.py
+++ functions/mal.py
@@ -7,38 +7,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 
 from functions.roulettes import roulette
+from utils.utils import *
 
-class Statuses(Enum):
-    ALL_ANIME = 0
-    CURRENTLY_WATCHING = 1
-    COMPLETED = 2
-    ON_HOLD = 3
-    DROPPED = 4
-    PLAN_TO_WATCH = 6
-
-bot_channel_id = 571462116612112384    # Channel ID where announcements will be sent
-
-mal_profiles = 'data/mal_profile.txt'
-currently_watching = f'data/anime_list/{Statuses.CURRENTLY_WATCHING.value}.txt'
-plan_to_watch = f'data/anime_list/{Statuses.PLAN_TO_WATCH.value}.txt'
-files = [mal_profiles, currently_watching, plan_to_watch]
-
-def check_if_file_exist(file_path):
-    os.makedirs(os.path.dirname(file_path), exist_ok=True)
-    if not os.path.exists(file_path):
-        with open(file_path, 'w', encoding='utf-8') as f:
-            pass
-
-for file in files:
-    check_if_file_exist(file)
-
-def read_options(file_path):
-    with open(file_path, "r", encoding='utf-8') as file:
-        return [line.strip() for line in file.readlines()]
-
-def write_options(file_path, options):
-    with open(file_path, "w", encoding='utf-8') as file:
-        file.write("\n".join(options))
 
 async def mal_menu(interaction: discord.Interaction, action, user: str = None):
     if action.value == "add_user":
@@ -63,51 +33,53 @@ async def anime_list_menu(bot, interaction: discord.Interaction, action, user: s
         await interaction.response.send_message("Invalid option selected.", ephemeral=True)
 
 async def next_anime(interaction: discord.Interaction):
-    file_address = f'data/anime_list/{Statuses.PLAN_TO_WATCH.value}.txt'
-    anime_list = read_options(file_address)
+    file_address = MAL_STATUSES_TEMPLATE.substitute(status=Statuses.PLAN_TO_WATCH.value)
+
+    anime_list = load_text_data(file_address)
     anime_roulete_string = ",".join(anime_list)
     await roulette(interaction, anime_roulete_string)
 
 async def add_users_mal(interaction, user: str):
-    lines = read_options(mal_profiles)
+    lines = load_text_data(MAL_PROFILE_PATH)
     lines.append(user.strip())
-    write_options(mal_profiles, lines)
+    save_text_data(MAL_PROFILE_PATH, lines)
     await interaction.response.send_message(f"Added the new user: {user.strip()}")
 
 async def view_users_mal(interaction):
-    lines = read_options(mal_profiles)
+    lines = load_text_data(MAL_PROFILE_PATH)
     await interaction.response.send_message(f"Users: {', '.join(lines)}")
 
 async def remove_users_mal(interaction, user: str):
-    lines = read_options(mal_profiles)
+    lines = load_text_data(MAL_PROFILE_PATH)
     lines.remove(user.strip())
-    write_options(mal_profiles, lines)
+    save_text_data(MAL_PROFILE_PATH, lines)
     await interaction.response.send_message(f"Removed the new option set: {user.strip()}")
 
 async def update_anime_list(bot, interaction, status):
     await interaction.response.send_message(f"Will be updated.")
 
-    file_address = f'data/anime_list/{status}.txt'
-    users = read_options(mal_profiles)
+    file_address = MAL_STATUSES_TEMPLATE.substitute(status=status)
+
+    users = load_text_data(MAL_PROFILE_PATH)
     titles = []
 
     for user in users:
         titles.extend(scrape(user, status))
 
-    write_options(file_address, titles)
+    save_text_data(file_address, titles)
 
-    channel = bot.get_channel(bot_channel_id)
+    channel = bot.get_channel(BOT_CHANNEL_ID)
     name = Statuses(status).name
 
     await channel.send(f"Finish to update {name} list.")
 
 
 async def view_anime_list(interaction, status):
-    max_letters = 2000
-    file_address = f'data/anime_list/{status}.txt'
-    anime_list = read_options(file_address)
+    file_address = MAL_STATUSES_TEMPLATE.substitute(status=status)
+
+    anime_list = load_text_data(file_address)
     if len(anime_list) > 0:
-        await interaction.response.send_message("\n".join(anime_list)[:max_letters])
+        await interaction.response.send_message("\n".join(anime_list)[:MAX_LETTERS])
     else:
         await interaction.response.send_message("No anime.")
 
@@ -140,6 +112,7 @@ def scrape(user, status):
         except Exception as e:
             print(f"Error parsing row: {e}")
     print('Finish scrape.')
+
     return titles
 
     # Quit the driver
